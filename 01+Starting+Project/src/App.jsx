883495c73db63d59868b1eeef964e4f2eda7bf71
jairosam <jairobuitrago3@gmail.com>
2025-05-31 18:53:51 -0500

    fixing bug

--- 01+Starting+Project/src/App.jsx
+++ 01+Starting+Project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>I'm learning & Mastering GitHub Actions</h1>
+        <h1>Im learning & Mastering GitHub Actions</h1>
       </header>
       <MainContent />
     </>
